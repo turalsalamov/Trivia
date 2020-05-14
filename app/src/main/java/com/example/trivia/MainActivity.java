43d80963f8c2f734698e56dcd07c3118e0c07d35
zalza <turalsalamov109@gmail.com>
2020-05-14 23:07:00 +0400

    timer score

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -16,13 +20,15 @@ import android.widget.Toast;
 import com.example.trivia.data.AnswerListAsyncResponse;
 import com.example.trivia.data.QuestionBank;
 import com.example.trivia.model.Question;
+import android.app.AlertDialog;
 
 import java.util.ArrayList;
 
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView scoreText;
+    private static int score;
     private static int number;
     private ArrayList<Question> questionArrayList = new ArrayList<>();
     private TextView question;
@@ -30,19 +36,25 @@ public class MainActivity extends AppCompatActivity {
     private Button previous;
     private Button next;
     private CardView cardView;
-
+    private Double timer = 30.00;
+    private  TextView timerText;
+    private Handler handler;
+    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         number = 1;
-
+        score = 0;
         counter = findViewById(R.id.counter);
         question = findViewById(R.id.question);
         previous = findViewById(R.id.previous);
         next = findViewById(R.id.next);
         cardView = findViewById(R.id.cardview);
+        scoreText = findViewById(R.id.score);
+        timerText = findViewById(R.id.time);
+
 
         questionArrayList.clear();
         new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
@@ -50,11 +62,40 @@ public class MainActivity extends AppCompatActivity {
             public void processFinished(ArrayList<Question> arrayList) {
                 questionArrayList = arrayList;
                 updateQuestionAndNumberOfThem();
-                if (number == 1){
+                new CountDownTimer(31000, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        Long time = millisUntilFinished/1000;
+                        timerText.setText(time.toString());
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        AlertDialog.Builder builder =  new AlertDialog.Builder(MainActivity.this).setTitle("Again").setMessage("Do you want to play again?").
+                                setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        recreate();
+                                    }
+                                }).
+                                setNegativeButton("Quit", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        int pid = android.os.Process.myPid();
+                                        android.os.Process.killProcess(pid);
+                                    }
+                                });
+                        builder.create();
+                        builder.show();
+
+                    }
+                }.start();
+                if (number == 1) {
                     previous.setEnabled(false);
                 }
             }
         });
+
 
     }
 
@@ -115,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    
+
     public void updateQuestionAndNumberOfThem(){
         question.setText(questionArrayList.get(number-1).getQuestion());
         counter.setText(number + "/" + questionArrayList.size());
@@ -130,11 +171,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAnimationStart(Animation animation) {
                 cardView.setCardBackgroundColor(Color.GREEN);
+                score();
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
                 cardView.setCardBackgroundColor(getResources().getColor(R.color.card,null));
+                afterAnswer();
             }
 
             @Override
@@ -157,6 +200,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAnimationEnd(Animation animation) {
                 cardView.setCardBackgroundColor(getResources().getColor(R.color.card,null));
+                afterAnswer();
             }
 
             @Override
@@ -165,6 +209,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void afterAnswer(){
+        questionArrayList.remove(number-1);
+        updateQuestionAndNumberOfThem();
+    }
+
+    public void score(){
+        score++;
+        scoreText.setText("Score: " + score);
     }
 
 }
